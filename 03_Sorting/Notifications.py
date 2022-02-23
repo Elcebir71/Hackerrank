487fe17e376a47ef63017d408de17bae891a13aa
Toshiba_PC <hakanshah71@gmail.com>
2022-02-23 20:54:29 +0300

    last change

--- 03_Sorting/Notifications.py
+++ 03_Sorting/Notifications.py
@@ -5,7 +5,6 @@ import os
 import random
 import re
 import sys
-from collections import deque
 
 
 #
@@ -22,63 +21,55 @@ def activityNotifications(expenditure, d):
     k = [i for i in expenditure[:d]]
     k.sort()
     ind = d // 2
+    print(ind)
     if d % 2 != 0:
         for indx, arr in enumerate(expenditure[d:]):
+
             if 0 < 2 * k[ind] <= arr:
                 count += 1
                 k.pop(0)
-                ordinarySort(k, arr)
+                ordinary_sort(k, arr)
             else:
                 k.pop(0)
-                ordinarySort(k, arr)
+                ordinary_sort(k, arr)
     else:
         for indx, arr in enumerate(expenditure[d:]):
-            if 0 <= k[ind] + k[ind + 1] <= arr:
+
+            if 0 <= k[ind] + k[ind - 1] <= arr:
                 count += 1
                 k.pop(0)
-                ordinarySort(k, arr)
+                ordinary_sort(k, arr)
             else:
                 k.pop(0)
-                ordinarySort(k, arr)
+
+                ordinary_sort(k, arr)
+    print(count)
+    print(k[-1])
     return count
 
-def ordinarySort(lst, element):
+
+def ordinary_sort(lst, element):
     if element >= lst[-1]:
         lst.append(element)
     elif element <= lst[0]:
         lst.insert(0, element)
     else:
-        m, times = len(lst), 0
-        indis, half = [m // 2], m // 2
-        if element - lst[indis[0]] > 0:
-            while element - lst[indis[-1]] > 0:
-                half = half // 2
-                if indis[-1] + half <= m:
-                    indis.append(indis[-1] + half)
-                else:
-                    break
-            if indis[-1] - indis[-2] > 1:
-                last1 = [ind2 for ind2, x in enumerate(lst[indis[-2]:indis[-1]]) if x <= element]
-
-                lst.insert(indis[-2] + last1[-1] + 1, element)
-            else:
-                lst.insert(indis[-1] + 1, element)
-        else:
-            while element - lst[indis[-1]] <= 0:
-                indis.append(indis[-1] // 2)
-            if indis[-2] - indis[-1] > 1:
-                last1 = [ind2 for ind2, x in enumerate(lst[indis[-1]:indis[-2]]) if x >= element]
-
-                lst.insert(indis[-1] + last1[-1] + 1, element)
-            else:
-                lst.insert(indis[-1] + 1, element)
-
+        start,  lng = 0, len(lst)
+        lng = len(lst) if len(lst) < 1000 else int(round(lng, -2)) + 1
+        steps = 1 if lng < 1000 else int(round(len(lst) / 10, -2))
+        while any([element - x <= 0 for x in lst[start:lng:steps]]):
+            indis = [x for x in range(start, lng, steps)]
+            indis_boolean = [element - x <= 0 for x in lst[start:lng:steps]]
+            start, lng = indis[indis_boolean.index(True) - 1], indis[indis_boolean.index(True)]
+            steps = int(round((lng - start) / 10, -2))
+            if steps <= 100:
+                lst.insert(start + [element - x <= 0 for x in lst[start:lng+1]].index(True), element)
+                break
     return lst
 
 
 if __name__ == '__main__':
-    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
     first_multiple_input = input().rstrip().split()
 
     n = int(first_multiple_input[0])
@@ -89,6 +80,6 @@ if __name__ == '__main__':
 
     result = activityNotifications(expenditure, d)
 
-    #fptr.write(str(result) + '\n')
+    # fptr.write(str(result) + '\n')
 
-    #fptr.close()
+    # fptr.close()
