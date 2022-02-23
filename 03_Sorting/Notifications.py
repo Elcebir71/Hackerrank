6f07160921a93793efe7449ba35e40ad0504d9ac
Toshiba_PC <hakanshah71@gmail.com>
2022-02-23 22:37:53 +0300

    last change

--- 03_Sorting/Notifications.py
+++ 03_Sorting/Notifications.py
@@ -5,7 +5,7 @@ import os
 import random
 import re
 import sys
-
+from collections import Counter
 
 #
 # Complete the 'activityNotifications' function below.
@@ -17,55 +17,56 @@ import sys
 #
 
 def activityNotifications(expenditure, d):
-    count, last, sum_lst, k = 0, 0, 0, []
-    k = [i for i in expenditure[:d]]
-    k.sort()
+    count, k = 0, []
+    k = expenditure
+    m = k[:d].copy()
+    m.sort()
     ind = d // 2
-    print(ind)
+
     if d % 2 != 0:
         for indx, arr in enumerate(expenditure[d:]):
-
-            if 0 < 2 * k[ind] <= arr:
+            if 0 <= 2 * m[ind] <= arr:
                 count += 1
-                k.pop(0)
-                ordinary_sort(k, arr)
+                m.pop(m.index(k[indx]))
+                ordinary_sort(k, m, arr)
             else:
-                k.pop(0)
-                ordinary_sort(k, arr)
+                m.pop(m.index(k[indx]))
+                ordinary_sort(k, m, arr)
     else:
         for indx, arr in enumerate(expenditure[d:]):
 
-            if 0 <= k[ind] + k[ind - 1] <= arr:
+            if 0 <= m[ind] + m[ind - 1] <= arr:
                 count += 1
-                k.pop(0)
-                ordinary_sort(k, arr)
+                m.pop(m.index(k[indx]))
+                ordinary_sort(k, m, arr)
             else:
-                k.pop(0)
+                print(indx)
 
-                ordinary_sort(k, arr)
+                m.pop(m.index(k[indx]))
+                ordinary_sort(k, m, arr)
     print(count)
-    print(k[-1])
+    print(ind)
     return count
 
 
-def ordinary_sort(lst, element):
-    if element >= lst[-1]:
-        lst.append(element)
-    elif element <= lst[0]:
-        lst.insert(0, element)
+def ordinary_sort(lst, srt, element):
+    if element >= srt[-1]:
+        srt.append(element)
+    elif element <= srt[0]:
+        srt.insert(0, element)
     else:
-        start,  lng = 0, len(lst)
-        lng = len(lst) if len(lst) < 1000 else int(round(lng, -2)) + 1
-        steps = 1 if lng < 1000 else int(round(len(lst) / 10, -2))
-        while any([element - x <= 0 for x in lst[start:lng:steps]]):
+        start,  lng = 0, len(srt)
+        lng = len(srt) if len(srt) < 1000 else int(round(lng, -2)) + 1
+        steps = 1 if lng < 1000 else int(round(len(srt) / 10, -2))
+        while any([element - x <= 0 for x in srt[start:lng:steps]]):
             indis = [x for x in range(start, lng, steps)]
-            indis_boolean = [element - x <= 0 for x in lst[start:lng:steps]]
+            indis_boolean = [element - x <= 0 for x in srt[start:lng:steps]]
             start, lng = indis[indis_boolean.index(True) - 1], indis[indis_boolean.index(True)]
             steps = int(round((lng - start) / 10, -2))
             if steps <= 100:
-                lst.insert(start + [element - x <= 0 for x in lst[start:lng+1]].index(True), element)
+                srt.insert(start + [element - x <= 0 for x in srt[start:lng + 1]].index(True), element)
                 break
-    return lst
+    return srt
 
 
 if __name__ == '__main__':
