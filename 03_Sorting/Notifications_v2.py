a27cd6a3d98e6c320cb0826ff8841b45728d3813
Toshiba_PC <hakanshah71@gmail.com>
2022-02-26 23:43:35 +0300

    last change

--- 03_Sorting/Notifications_v2.py
+++ 03_Sorting/Notifications_v2.py
@@ -3,6 +3,10 @@ import os
 import random
 import re
 import sys
+import time
+from collections import Counter,defaultdict
+
+
 #
 # Complete the 'activityNotifications' function below.
 #
@@ -13,10 +17,48 @@ import sys
 #
 
 def activityNotifications(expenditure, d):
-    count, k = 0, []
+    start1 = time.process_time()
+    count, med, s, counter = 0, d // 2, 0, defaultdict(int)
+    rng = set(expenditure[:d])
+    counter = {x: expenditure[:d].count(x) for x in rng}
+    counter = defaultdict(int,counter)
+    count_ord = {key: (s := s + val) for key, val in counter.items()}
 
+    if d % 2 == 0:
+        for index, arg in enumerate(expenditure[d:]):
+            med_dn = [[x, y] for x, y in count_ord.items() if y <= med][-1]
+            med_up = [[x, y] for x, y in count_ord.items() if y > med][0]
+            pop1 = expenditure[index]
+            if med_dn[1] < med:
+                med1 = med2 = med_up[0]
+            else:
+                med1, med2 = med_dn[0], med_up[0]
+            if med1+med2 <= arg:
+                count += 1
+            counter[pop1] -= 1
+            counter[arg] += 1
+            s = 0
+            count_ord = {key: (s := s + val) for key, val in counter.items()}
+    else:
+        for index, arg in enumerate(expenditure[d:]):
+            med_dn = [[x, y] for x, y in count_ord.items() if y <= med][-1]
+            med_up = [[x, y] for x, y in count_ord.items() if y > med][0]
+            pop1 = expenditure[index]
+            if med_dn[1] <= med-1:
+                med1 = med_dn[0]
+            else:
+                med1 = med_up[0]
+            if 2 * med1 <= arg:
+                count += 1
+            counter[pop1] -= 1
+            counter[arg] += 1
+            s = 0
+            count_ord = {key: (s := s + val) for key, val in counter.items()}
 
-
+    finish = time.process_time()
+    tm = (finish - start1)
+    print(count, tm)
+    return count
 
 
 if __name__ == '__main__':
