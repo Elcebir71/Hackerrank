7cacaf5098004a885978cf29057caba24652292b
Toshiba_PC <hakanshah71@gmail.com>
2022-03-11 09:58:06 +0300

    last change

--- String  Manipulation/Special String Again.py
+++ String  Manipulation/Special String Again.py
@@ -5,13 +5,23 @@ import re
 import sys
 from collections import defaultdict
 
+
 # Complete the substrCount function below.
 def substrCount(n, s):
-    strng = defaultdict(list)
-    for ind, char in enumerate(s):
-        strng[char] += [ind]
-    sub = 'aa'
-    sum(1 for _ in s.startwith(sub))
+    cnt = n
+    for i in range(n):
+        j = i
+        while j < n - 1:
+            j += 1
+            if s[j] == s[i]:
+                cnt += 1
+            else:
+                if s[i:j] == s[j + 1 : 2 * j - i + 1]:
+                    cnt += 1
+                break
+
+    return cnt
+
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -21,7 +31,7 @@ if __name__ == '__main__':
     s = input()
 
     result = substrCount(n, s)
-
+    print(result)
     # fptr.write(str(result) + '\n')
 
     # fptr.close()
